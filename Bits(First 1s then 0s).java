8c4a2bf52199f2ff50cb9ceea431bd02655120d5
karan9915 <36379263+karan9915@users.noreply.github.com>
2020-03-30 13:03:07 +0530

    Add files via upload

---
+++ Bits(First 1s then 0s).java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+class Main
+{
+
+	void Bits(String str)
+	{
+	   char[] bit = str.toCharArray();
+       int size = bit.length;
+       char[] x = new char[size];
+       int left = 0;
+       int right = size-1;
+       for(int i=size-1;i>=0;i--)
+       {
+       	    if(bit[i]=='1')
+       	    {
+       		    x[left] = bit[i]; 
+       		    left++;
+        	}
+        	if(bit[i]=='0')
+        	{
+       		    x[right] = bit[i];
+       		    right--;
+        	}
+       }
+
+       for(int i=0;i<size;i++)
+       {
+        	System.out.print(x[i]);
+       }
+	}
+
+	public static void main(String[] args)
+	{
+       String str = "110001010101";
+       Main m = new Main();
+       m.Bits(str);
+       
+    }	
+}
