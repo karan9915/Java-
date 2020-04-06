c91964a270724569ca434ba606a840821149f794
karan9915 <36379263+karan9915@users.noreply.github.com>
2020-04-06 22:32:50 +0530

    Add files via upload

---
+++ ArmStrong No..java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class ll
+{
+	public static void main(String[] args)
+	{
+      Scanner scan = new Scanner(System.in);
+      int num = scan.nextInt();
+      int n = num;
+      int b = 0;
+     while(n!=0)
+     {
+       int a = n%10;
+      b+=a*a*a;
+      
+       n=n/10;
+     }
+     if(num==b)
+     {
+     	 System.out.println("Armstrong No.");
+     }
+     else
+     {
+     	System.out.println("Not");
+     }
+    
+    }	
+}
